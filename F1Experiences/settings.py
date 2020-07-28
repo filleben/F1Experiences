2a26a52b106893b168e43dbcbf2e7b03adfdc73a
filleben <filleryben@gmail.com>
2020-07-28 11:32:47 +0100

    fixed bug with importing env.py

--- F1Experiences/settings.py
+++ F1Experiences/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-if path.exists("env.py"):
-  import env 
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
