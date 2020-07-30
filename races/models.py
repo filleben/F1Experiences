f1a1a7e0064212378576a3e19c7ea91db3f7a841
filleben <filleryben@gmail.com>
2020-07-30 23:46:10 +0100

    Added Race model

--- races/models.py
+++ races/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Race(models.Model):
+    name = models.CharField(max_length=400)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    date = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+    def get_date(self):
+        return self.date
