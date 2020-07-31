4ba5e955a50689168fc56af023398b1caef05c1f
filleben <filleryben@gmail.com>
2020-07-31 13:36:51 +0100

    Added ticket model

--- races/models.py
+++ races/models.py
@@ -15,3 +15,12 @@ class Race(models.Model):
     
     def get_date(self):
         return self.date
+
+class Ticket(models.Model):
+    race = models.ForeignKey('Race', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
