d9a932bf62ce895e996c9ef5d0e9bc4c0a70818e
filleben <filleryben@gmail.com>
2020-08-01 00:56:37 +0100

    added view to view race objects

--- races/views.py
+++ races/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Race
 
 # Create your views here.
+
+def all_races(request):
+    races = Race.objects.all()
+    context = {
+        'races': races,
+    }
+    return render(request, 'races/races.html', context)
