1136ac1352b8ba6a7472f741cf265973a17b0cc5
filleben <filleryben@gmail.com>
2020-08-01 17:16:09 +0100

    added race images to model

--- races/models.py
+++ races/models.py
@@ -6,6 +6,8 @@ class Race(models.Model):
     name = models.CharField(max_length=400)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     date = models.CharField(max_length=20)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
