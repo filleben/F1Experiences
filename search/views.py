e9288b36dd36f34ed716d12c08a6ac17ab6c2c4f
filleben <filleryben@gmail.com>
2020-08-07 13:57:57 +0100

    created search view

--- search/views.py
+++ search/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from races.models import Race
 
 # Create your views here.
+
+def search(request):
+    if request.method == "GET":
+        races = Race.objects.filter(friendly_name__icontains=request.GET['q'])
+        context = {
+            'races': races
+        }
+        return render(request, 'races/races.html', context)
