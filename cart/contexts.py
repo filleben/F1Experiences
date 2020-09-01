e42c48f86609c80b2856982a4e3bcc35b7d84ff0
filleben <filleryben@gmail.com>
2020-09-01 16:07:58 +0100

    updated cart contents function

--- cart/contexts.py
+++ cart/contexts.py
@@ -6,6 +6,17 @@ def cart_contents(request):
     cart_items = []
     total = 0
     ticket_count = 0
+    cart = request.session.get('cart', {})
+
+    for id, quantity in cart.items():
+        ticket = get_object_or_404(Ticket, pk=id)
+        total += quantity * ticket.price
+        ticket_count += quantity
+        cart_items.append({
+            'id': id,
+            'quantity': quantity,
+            'ticket': ticket,
+        })
 
     context = {
         'cart_items': cart_items,
