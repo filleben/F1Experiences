2812cdefe42e26765e3d7d11c5cfc3d8502c788a
filleben <filleryben@gmail.com>
2020-09-09 12:06:23 +0100

    added tests for races models

--- races/tests.py
+++ races/tests.py
@@ -58,3 +58,55 @@ class TestURLs(SimpleTestCase):
         url = reverse('race_detail', args=[1])
         print(resolve(url))
         self.assertEquals(resolve(url).func, race_details)
+
+class TestModels(TestCase):
+    def test_race_model(self):
+        """
+        Testing Race model is passing values correctly
+        """
+        race = Race.objects.create(
+            name='test_race',
+            friendly_name='test_friendly_name',
+            date='test_date',
+            image_url='test_image_URL',
+            image='test.png',
+            flag_url='test_flag_URL',
+            flag='flag.png',
+            location='test_location',
+            race_views='0',
+        )
+        race.save()
+        self.assertEquals(race.name, 'test_race')
+        self.assertEquals(race.friendly_name, 'test_friendly_name')
+        self.assertEquals(race.date, 'test_date')
+        self.assertEquals(race.image_url, 'test_image_URL')
+        self.assertEquals(race.image, 'test.png')
+        self.assertEquals(race.flag_url, 'test_flag_URL')
+        self.assertEquals(race.flag, 'flag.png')
+        self.assertEquals(race.location, 'test_location')
+        self.assertEquals(race.race_views, '0')
+
+    def test_ticket_model(self):
+        """
+        Testing Ticket model is passing values correctly and related to the Race model
+        """
+        race = Race.objects.create(
+            name='test_race',
+            friendly_name='test_friendly_name',
+            date='test_date',
+            image_url='test_image_URL',
+            image='test.png',
+            flag_url='test_flag_URL',
+            flag='flag.png',
+            location='test_location',
+            race_views='0',
+        )
+        race.save()
+        ticket = Ticket.objects.create(
+            race=race,
+            name='test_ticket',
+            description='test_description',
+            price='0.0'
+        )
+        ticket.save()
+        self.assertEquals(ticket.race, race)
