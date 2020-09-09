7698db56a20792fe9789eafcea4b0e826f8ebff2
filleben <filleryben@gmail.com>
2020-09-09 14:10:43 +0100

    added line item model, imported required modules

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,8 @@
+import uuid
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+from races.models import Race, Ticket
 
 class Order(models.Model):
     order_number = models.CharField(max_length=16, null=False, editable=False)
@@ -13,3 +17,9 @@ class Order(models.Model):
     county = models.CharField(max_length=40, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    ticket = models.ForeignKey(Ticket, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
