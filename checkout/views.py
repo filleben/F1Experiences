14df3c574e828d079d83fbee4bd45de3d06bbd90
filleben <filleryben@gmail.com>
2020-09-10 11:15:51 +0100

    created basic checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect , reverse
+from django.contrib import messages
 
-# Create your views here.
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Your cart is empty")
+        return redirect(reverse('races'))
+
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form
+    }
+
+    return render (request, 'checkout/checkout.html', context)
