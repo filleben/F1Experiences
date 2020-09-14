4a8b342562db083ad977442dcd8def47244e826f
filleben <filleryben@gmail.com>
2020-09-14 20:40:13 +0100

    created stripe payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -2,17 +2,34 @@ from django.shortcuts import render, redirect , reverse
 from django.contrib import messages
 from .forms import OrderForm
 from django.conf import settings
+from cart.contexts import cart_contents
+import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "Your cart is empty")
         return redirect(reverse('races'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency='gbp',
+    )
+
+    print(intent)
+
     order_form = OrderForm()
     context = {
         'order_form': order_form,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render (request, 'checkout/checkout.html', context)
