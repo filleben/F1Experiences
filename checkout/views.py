ff0c362f4ca879315b429fde25f2a1957c2d66fd
filleben <filleryben@gmail.com>
2020-09-14 14:27:46 +0100

    tested stripe public key was working as expected

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect , reverse
 from django.contrib import messages
 from .forms import OrderForm
+from django.conf import settings
 
 def checkout(request):
     cart = request.session.get('cart', {})
@@ -10,7 +11,8 @@ def checkout(request):
 
     order_form = OrderForm()
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY
     }
 
     return render (request, 'checkout/checkout.html', context)
