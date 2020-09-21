6264cbb9b45368dc06ec67cd3c0203f85b1b8714
filleben <filleryben@gmail.com>
2020-09-21 14:15:36 +0100

    added troubleshooting print statements

--- checkout/singals.py
+++ checkout/singals.py
@@ -4,8 +4,10 @@ from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
+    print('save singal received!')
     instance.order.update_total()
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
+    print('delete singal received!')
     instance.order.update_total()
