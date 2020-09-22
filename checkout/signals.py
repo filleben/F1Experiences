19e2e4cbbd3ddf98ec930108c7fbded031fa7355
filleben <filleryben@gmail.com>
2020-09-22 11:30:33 +0100

    fixed typo, fixed order totals not updating

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-from .models import OrderLineItem
+from .models import OrderLineItem, Order
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
