fc20a4caebde242056c1b502244c04263a96b163
filleben <filleryben@gmail.com>
2020-09-28 15:23:13 +0100

    finished webhook handler and checkout view

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
+from .models import Order, OrderLineItem
+from races.models import Ticket
+import json
+import time
 
 class StripeWebhook_Handler:
     """
@@ -22,9 +26,77 @@ class StripeWebhook_Handler:
         Handle payment_intent.succeeded webhook from stripe
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        cart = intent.metadata.cart
+        save_info = intent.metadata.save_info
+        billing_details = intent.charges.data[0].billing_details
+        order_total = round(intent.charges.data[0].amount / 100, 2)
+
+        for field, value in billing_details.address.items():
+            if value == "":
+                billing_details.address[field] = None
+
+        order_exists = False
+        attempt = 1
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=billing_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=billing_details.phone,
+                    street_address1__iexact=billing_details.address.line1,
+                    street_address2__iexact=billing_details.address.line2,
+                    town_or_city__iexact=billing_details.address.city,
+                    county__iexact=billing_details.address.state,
+                    country__iexact=billing_details.address.country,
+                    postcode__iexact=billing_details.address.postal_code,
+                    order_total=order_total,
+                    original_cart=cart,
+                    stripe_pid=pid,
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook Received: {event["type"]} | SUCCESS: Order has been created',
+                status=200
+            )
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=billing_details.name,
+                    email=billing_details.email,
+                    phone_number=billing_details.phone,
+                    street_address1=billing_details.address.line1,
+                    street_address2=billing_details.address.line2,
+                    town_or_city=billing_details.address.city,
+                    county=billing_details.address.state,
+                    country=billing_details.address.country,
+                    postcode=billing_details.address.postal_code,
+                    original_cart=cart,
+                    stripe_pid=pid,
+                )
+                for item_id, item_data in json.loads(cart).items():
+                    ticket = Ticket.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        ticket=ticket,
+                        quantity=item_data,
+                    )
+                    order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook Received: {event["type"]} | ERROR: {e}',
+                    status=500
+                )
         return HttpResponse(
-            content=f'Webhook Received: {event["type"]}',
+            content=f'Webhook Received: {event["type"]} | SUCCESS: Order created in webhook',
             status=200
         )
 
