9e5b24bf7260aacc04f25b4e7519e4788b3deb54
filleben <filleryben@gmail.com>
2020-09-29 22:20:50 +0100

    changed country field django countries field

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django_countries.fields import CountryField
 from races.models import Race, Ticket
 
 class Order(models.Model):
@@ -9,7 +10,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     town_or_city = models.CharField(max_length=40, blank=False)
     street_address1 = models.CharField(max_length=40, blank=False)
     street_address2 = models.CharField(max_length=40, blank=True)
