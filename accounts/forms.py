4a49c94479467797cf45a5429710ed55806b9658
filleben <filleryben@gmail.com>
2020-10-02 12:28:37 +0100

    created profile form

---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import UserProfile
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+        labels = {
+            'default_phone_number': 'Phone Number',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+            'default_county': 'County/State',
+            'default_country': 'Country',
+        }
