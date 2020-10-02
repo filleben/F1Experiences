94fd1e4c08019399a82a00eedf9b01708dddfc8e
filleben <filleryben@gmail.com>
2020-10-02 12:29:44 +0100

    updated profile view to handle profile updates

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile Updated')
+
+    form = UserProfileForm(instance=profile)
     context = {
-        'profile': profile
+        'profile': profile,
+        'form': form,
     }
 
     return render(request, 'accounts/profile.html', context)
