8e8c9094ee566b49d10c58b15722d86fc19f010e
filleben <filleryben@gmail.com>
2020-10-21 10:16:55 +0100

    added url for edit race

--- races/urls.py
+++ races/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('<int:race_id>/', views.race_details, name='race_detail'),
     path('add_race/', views.add_race, name='add_race'),
     path('add_ticket/', views.add_ticket, name='add_ticket'),
+    path('edit_race/<int:race_id>/', views.edit_race, name='edit_race'),
 ]
