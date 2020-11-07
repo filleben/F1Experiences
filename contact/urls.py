f80d4c264b542eee5ea9b5c64dbaa87e5d3d79b0
filleben <filleryben@gmail.com>
2020-11-07 20:47:13 +0000

    updated contact url, added contact success url

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('contact/', views.contact, name='contact'),
+    path('', views.contact, name='contact'),
+    path('contact_success/<contact_number>', views.contact_success, name='contact_success'),
 ]
