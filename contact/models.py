3def9edc0f4da8e3897259d2a281ec636287c1ed
filleben <filleryben@gmail.com>
2020-11-07 12:57:35 +0000

    created contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=254, null=False, blank=False)
+    last_name = models.CharField(max_length=254, null=False, blank=False)
+    contact_email = models.EmailField(max_length=254, null=False, blank=False)
+    contact_phone = models.CharField(max_length=20, null=False, blank=False)
+    subject = models.CharField(max_length=254, null=False, blank=False)
+    message = models.CharField(max_length=1000, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
