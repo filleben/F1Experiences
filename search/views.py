c3a61ac51b7c7126d626beb37e0579a27ce926f8
filleben <filleryben@gmail.com>
2020-11-12 10:41:23 +0000

    added event management search, renamed other search views

--- search/views.py
+++ search/views.py
@@ -3,7 +3,7 @@ from races.models import Race, Ticket
 
 # Create your views here.
 
-def search(request):
+def event_search(request):
     if request.method == "GET":
         races = Race.objects.filter(friendly_name__icontains=request.GET['q'])
         context = {
@@ -11,10 +11,18 @@ def search(request):
         }
         return render(request, 'races/races.html', context)
 
-def ticket_search(request):
+def ticket_management_search(request):
     if request.method == "GET":
         tickets = Ticket.objects.filter(name__icontains=request.GET['q'])
         context = {
             'tickets': tickets
         }
         return render(request, 'races/ticket_management.html', context)
+
+def event_management_search(request):
+    if request.method == "GET":
+        races = Race.objects.filter(friendly_name__icontains=request.GET['q'])
+        context = {
+            'races': races
+        }
+        return render(request, 'races/event_management.html', context)
