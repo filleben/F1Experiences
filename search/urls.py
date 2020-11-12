14960ac2681e3a73535eca33f8c38b9293d473d7
filleben <filleryben@gmail.com>
2020-11-12 10:41:33 +0000

    updated search urls

--- search/urls.py
+++ search/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('races/', views.search, name='search'),
-    path('ticket_management/', views.ticket_search, name='ticket_search'),
+    path('races/', views.event_search, name='event_search'),
+    path('ticket_management/', views.ticket_management_search, name='ticket_management_search'),
+    path('event_management/', views.event_management_search, name='event_management_search'),
 ]
