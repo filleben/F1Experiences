bdbd09a720bee538c78cc26260e6a9c3aa81fa1a
filleben <filleryben@gmail.com>
2020-11-28 11:10:53 +0000

    added delete ticket url

--- races/urls.py
+++ races/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('edit_race/<int:race_id>/', views.edit_race, name='edit_race'),
     path('delete_race/<int:race_id>/', views.delete_race, name='delete_race'),
     path('edit_ticket/<int:ticket_id>/', views.edit_ticket, name='edit_ticket'),
+    path('delete_ticket/<int:ticket_id>/', views.delete_ticket, name='delete_ticket'),
     path('event_management/', views.event_management, name='event_management'),
     path('ticket_management/', views.ticket_management, name='ticket_management'),
 ]
