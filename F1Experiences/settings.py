07af5510492ee0ebfd0726fbc810525476b12512
filleben <filleryben@gmail.com>
2020-12-01 15:32:07 +0000

    fixed email auth error

--- F1Experiences/settings.py
+++ F1Experiences/settings.py
@@ -218,6 +218,6 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
     DEFAULT_CONTACT_NUMBER = '0123456789'
