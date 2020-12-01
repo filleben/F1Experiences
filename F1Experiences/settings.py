fe026cba72e1bcf753f645349c36a39dbe682794
filleben <filleryben@gmail.com>
2020-12-01 15:10:30 +0000

    setup live emails

--- F1Experiences/settings.py
+++ F1Experiences/settings.py
@@ -117,8 +117,6 @@ SITE_ID = 1
 
 # Authentication Settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
@@ -210,5 +208,16 @@ if 'USE_AWS' in os.environ:
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Email settings
-DEFAULT_FROM_EMAIL = 'orders@F1Experiences.com'
-DEFAULT_CONTACT_NUMBER = '0123456789'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'orders@F1Experiences.com'
+    DEFAULT_CONTACT_NUMBER = '0123456789'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_CONTACT_NUMBER = '0123456789'
