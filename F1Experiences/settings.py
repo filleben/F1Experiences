af362fc3becdf96bca769f6d7d51e9d8e294e8a2
filleben <filleryben@gmail.com>
2020-12-01 11:03:36 +0000

    configured and setup AWS storage

--- F1Experiences/settings.py
+++ F1Experiences/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'accounts',
     'contact',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -186,6 +187,22 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#AWS
+if 'USE_AWS' in os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'f1-experiences'
+    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFUALT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Email settings
 DEFAULT_FROM_EMAIL = 'orders@F1Experiences.com'
