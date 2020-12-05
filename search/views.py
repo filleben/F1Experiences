4f8be739a424128b6c220e6ef847cde8894c879c
filleben <filleryben@gmail.com>
2020-12-05 23:02:11 +0000

    added comments to code

--- search/views.py
+++ search/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from races.models import Race, Ticket
 
-# Create your views here.
-
+#Event Search
 def event_search(request):
+    """
+    Search race events
+    """
     if request.method == "GET":
         races = Race.objects.filter(friendly_name__icontains=request.GET['q'])
         context = {
@@ -11,7 +13,11 @@ def event_search(request):
         }
         return render(request, 'races/races.html', context)
 
+#Ticket Management Search
 def ticket_management_search(request):
+    """
+    Ticket management search for admin use
+    """
     if request.method == "GET":
         tickets = Ticket.objects.filter(name__icontains=request.GET['q'])
         context = {
@@ -19,7 +25,11 @@ def ticket_management_search(request):
         }
         return render(request, 'races/ticket_management.html', context)
 
+#Event Management Search
 def event_management_search(request):
+    """
+    Event management search for admin use
+    """
     if request.method == "GET":
         races = Race.objects.filter(friendly_name__icontains=request.GET['q'])
         context = {
