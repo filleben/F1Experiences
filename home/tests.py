5ebe1110e5ef4c94a7201227469576a1cdb196e4
filleben <filleryben@gmail.com>
2020-12-08 23:42:43 +0000

    created tests for home views and urls

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse, resolve
+from home.views import popular_races
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def test_popular_races(self):
+        """
+        Testing popular_races view
+        """
+        client = Client()
+        response = client.get(reverse('home'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_404_error_handler(self):
+        """
+        Testing 404_error_handler
+        """
+        response = self.client.get('something/really/weird/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, '404.html')
+
+
+class TestURLs(SimpleTestCase):
+    def test_home_URL(self):
+        """
+        Testing home URL
+        """
+        url = reverse('home')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, popular_races)
