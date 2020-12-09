9a14a3c2d53d4a6e59509ec163611c2f0ef08bf0
filleben <filleryben@gmail.com>
2020-12-09 12:16:32 +0000

    created tests for cart views and urls

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse, resolve
+from cart.views import view_cart, add_to_cart, remove_item
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    def test_view_cart(self):
+        """
+        Testing view_cart view
+        """
+        client = Client()
+        response = client.get(reverse('view_cart'))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cart/cart.html')
+
+
+class TestURLs(SimpleTestCase):
+    def test_cart_URL(self):
+        """
+        Testing cart URL
+        """
+        url = reverse('view_cart')
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, view_cart)
+
+    def test_add_to_cart_URL(self):
+        """
+        Testing add_to_cart URL
+        """
+        url = reverse('add_to_cart', args=[1])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, add_to_cart)
+
+    def test_remove_item_URL(self):
+        """
+        Testing remove_item URL
+        """
+        url = reverse('remove_item', args=[1])
+        print(resolve(url))
+        self.assertEquals(resolve(url).func, remove_item)
